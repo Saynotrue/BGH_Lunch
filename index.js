a64eee716b5d715ddafb80d1c51e874b0c449a9f
Saynotrue <163289222+Saynotrue@users.noreply.github.com>
2025-04-10 00:39:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/webhook', async (req, res) => {
   const gradeNum = grade.replace(/[^0-9]/g, '');
   const classNum = class_.replace(/[^0-9]/g, '');
 
-  const url = `https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531246&AY=2025&SEM=1&ALL_TI_YMD=${parsedDate}&GRADE=${grade}&CLASS_NM=${class_}`;
+  const url = `https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531246&AY=2025&SEM=1&ALL_TI_YMD=${parsedDate}&GRADE=${gradeNum}&CLASS_NM=${classNum}`;
 
   try {
     const response = await axios.get(url);
